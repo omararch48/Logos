6449420c598b4b4232df1615fe8e580709a37ded
Omar Esquivel <omardanielesquivel@gmail.com>
2022-03-09 18:56:22 -0600

    800px NavBar modificado

--- assets/js/app-sidebar.js
+++ assets/js/app-sidebar.js
@@ -7,7 +7,6 @@ console.log(buttonClicked);
 buttonClicked.addEventListener('click', () => {
 
     document.querySelector('.sidebar-menu').classList.toggle('active');
-    console.log('no mames');
 
     // document.querySelector('sidebar-menu').classList.toggle('.hidden');
 });
