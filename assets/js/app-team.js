853c9cc326d027e113924901c9492bdd75a06c82
Omar Esquivel <omardanielesquivel@gmail.com>
2022-03-17 04:49:05 -0600

    Team app css

--- assets/js/app-team.js
+++ assets/js/app-team.js
@@ -1,17 +1,32 @@
-const teamSelected = document.querySelectorAll('.team-person');
+let teamSelected = document.querySelector('.team-person-active');
+let teamSelection = document.querySelectorAll('.team-person');
+let infoSelection = document.querySelectorAll('.team-info');
+let cardSelection = document.querySelectorAll('.team-card');
+let imageSelection = document.querySelectorAll('.team-image img');
 
 
-teamSelected.forEach((teamName) => {
+infoSelection[0].style.color = 'var(--main-white)';
+cardSelection[0].style.display = 'flex';
+imageSelection[0].style.display = 'block';
 
+
+teamSelection.forEach((teamName) => {
     teamName.addEventListener('click', () => {
-
-        console.log(teamName);
-
+        for (let i = 0; i < teamSelection.length; i++) {
+            if (teamSelection[i] == teamName) {
+                teamSelection[i].classList.toggle('team-person-active');
+                infoSelection[i].style.color = 'var(--main-white)';
+                cardSelection[i].style.display = 'flex';
+                imageSelection[i].style.display = 'block';
+            }
+            if (teamSelection[i] == teamSelected) {
+                teamSelection[i].classList.toggle('team-person-active');
+                infoSelection[i].style.color = 'transparent';
+                cardSelection[i].style.display = 'none';
+                imageSelection[i].style.display = 'none';
+            }            
+        }   
+        teamSelected = document.querySelector('.team-person-active');
+        teamSelection = document.querySelectorAll('.team-person');
     });
-
-
-    // document.querySelector('.sidebar-menu').classList.toggle('active');
-
-    // document.querySelector('sidebar-menu').classList.toggle('.hidden');
-
 });
